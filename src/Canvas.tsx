d7f848aa7009a9420ca5a4c3b0432fb3bcd00ac4
Nathan Lopez <nathancarllopez@gmail.com>
2024-11-26 23:24:29 -0600

    Removed questions and added functionality for the Shape classes and CoordinateManager class

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,68 +1,39 @@
 import { useState, useRef, useEffect } from 'react';
+import { Shape, Point, Cursor, Axis, Geodesic } from './Shapes';
+import { CoordinatesManager } from './CoordinatesManager';
+import ToolbarConfig from './ToolbarConfig';
+import { CANVAS_BACKGROUND_COLOR } from './Constants';
 
-export default function Canvas() {
+export default function Canvas({ toolbarConfig }: { toolbarConfig: ToolbarConfig }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  /**
-   * Question:
-   * Here I want to initialize the canvas' dimensions based on the window dimensions.
-   * My motivation to separate it was that none of the drawings should depend on the window dimensions.
-   * Moreover, if I want this to work on mobile devices I suspect that this might be a good place to put code
-   * that changes the layout based on whether you're on a mobile device or not. Does that make sense?
-   * Should this be in its own component?
-   */
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    // window.resize event listener goes here
   }, []);
 
-  /**
-   * Question:
-   * Each of the shapes I'm drawing is right now an object with a "type" (a string) and some other properties (consisting of numbers or other shape objects).
-   * How do I get typescript to chill out about the way these objects are structured? The type declaration on the line that I define the shapes state, and I'm also getting a big complaint from VS Code when I call setShapes in the addLineWithClick function.
-   * Should I define a "Shapes" class and have each shape be an instance of that class? Or is there a different way that Typescript handles this?
-   */
-  const startingShapes = [
-    { type: 'axis', start: { x: 0, y: window.innerHeight * 0.95 }, end: { x: window.innerWidth, y: window.innerHeight * 0.95 } },
-    { type: 'axis', start: { x: window.innerWidth / 2, y: 0 }, end: { x: window.innerWidth / 2, y: window.innerHeight * 0.95 } },
-  ];
-  const [shapes, setShapes] = useState<{ type: string; [key: string]: any }[]>(startingShapes);
+  const leftInfinity = new Point(undefined, undefined, 0, window.innerHeight * 0.95);
+  const rightInfinity = new Point(undefined, undefined, window.innerWidth, window.innerHeight * 0.95);
+  const topInfinity = new Point(undefined, undefined, window.innerWidth / 2, 0);
+  const origin = new Point(0, 0);
+  const axes = [ new Axis(leftInfinity, rightInfinity), new Axis(topInfinity, origin)];
+  const [shapes, setShapes] = useState<Shape[]>(axes);
 
-  function drawShapes(context: CanvasRenderingContext2D) {
-    context.fillStyle = 'white';
+  function wipeCanvas(context: CanvasRenderingContext2D) {
+    context.fillStyle = CANVAS_BACKGROUND_COLOR;
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-
-    shapes.forEach((shape) => {
-      switch (shape.type) {
-        case 'axis': {
-          drawAxis(context, shape);
-          break;
-        }
-        case 'cursor': {
-          drawCursor(context, shape);
-          break;
-        }
-        case 'clickedPoint':
-        case 'point': {
-          drawPoint(context, shape);
-          break;
-        }
-        case 'geodesicCircle':
-        case 'geodesicVertical': {
-          drawGeodesic(context, shape);
-          break;
-        }
-      }
-    });
   }
 
-  /**
-   * Question:
-   * Does this useEffect hook make sense? I'm thinking that I want to redraw the canvas every time the shapes state changes. Is this the right way to do that? The React docs advise against using useEffect if you don't need it, so I want to make sure that I'm not doing something that's unnecessary. For example, couldn't I just call the drawShapes function whenever I call setShapes? That would be more direct, but I'm not sure if that's the "React" way to do it.
-   */
+  function drawAll(context: CanvasRenderingContext2D) {
+    wipeCanvas(context);
+    shapes.forEach(shape => shape.draw(context));
+  }
+  
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
@@ -70,149 +41,140 @@ export default function Canvas() {
     const context = canvas.getContext('2d');
     if (!context) return;
 
-    drawShapes(context);
+    drawAll(context); // Consider adding the drawAll code here if the logic stays simple
   }, [shapes]);
 
-  /**
-   * Question:
-   * I've defined this function and the next one inside the component for now for simplicitiy, but I think they ought to be passed as props from a parent component. This is because clicking and mouse moving will be used for other things in the future, i.e., they will be one of a few options that are selected by the user with a Toolbar component (that I have yet to write). Then I'll need some parent component to keep track of which tool is selected and pass the appropriate function to this component based on what the user has done. Does that make sense?
-   */
-  function displayCursorCoordinates(event: React.MouseEvent<HTMLCanvasElement>) {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
+  // function handleMouseMove(event: React.MouseEvent<HTMLCanvasElement>) {
+    // To do: if more mousemove logic is added, extend this function like how handleClick is extended
+  // }
 
-    const coordinates = getCoordFromMouse(canvas, event);
-    const shapesWithoutCursor = [...shapes].filter((shape) => shape.type !== 'cursor');
-    setShapes([...shapesWithoutCursor, { type: 'cursor', ...coordinates }]);
+  function displayCursorCoordinates(event: React.MouseEvent<HTMLCanvasElement>) {
+    const { canvasX, canvasY } = CoordinatesManager.getMouseCoordinates(event);
+    const shapesWithoutCursor = shapes.filter((shape) => shape.type !== 'Cursor');
+    const finalShapes = [...shapesWithoutCursor];
+
+    if (toolbarConfig.getToolbarValue('cursorCoordToggle')) {
+      finalShapes.push(new Cursor(undefined, undefined, canvasX, canvasY));
+    }
+
+    setShapes(finalShapes);
   }
 
-  function addLineWithClick(event: React.MouseEvent<HTMLCanvasElement>) {
+  function handleClick(event: React.MouseEvent<HTMLCanvasElement>) {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
-    const coordinates = getCoordFromMouse(canvas, event);
+    switch (toolbarConfig.getToolbarValue('clickTool')) {
+      case 'drawPoints': {
+        addPointWithClick(event);
+        break;
+      }
+      case 'drawGeodesics': {
+        addGeodesicWithClick(event);
+        break;
+      }
+    }
+  }
+
+  function addPointWithClick(event: React.MouseEvent<HTMLCanvasElement>) {
+    const coordinates = CoordinatesManager.getMouseCoordinates(event);
     if (coordinates.y < 0) return;
 
-    const clicked = { type: 'clickedPoint', ...coordinates };
-    const firstClicked = shapes.find((shape) => shape.type === 'clickedPoint');
+    setShapes([...shapes, new Point(coordinates.x, coordinates.y)]);
+  }
 
-    if (firstClicked) {
-      const newShapes = [
-        { ...firstClicked, type: 'point'},
-        { ...clicked, type: 'point' },
-        ...shapes.filter((shape) => shape.type !== 'clickedPoint'),
-        getGeodesic(canvas, firstClicked, clicked)
-      ];
-      setShapes(newShapes);
-    } else {
-      setShapes([...shapes, clicked]);
+  function addGeodesicWithClick(event: React.MouseEvent<HTMLCanvasElement>) {
+    const coordinates = CoordinatesManager.getMouseCoordinates(event);
+    if (coordinates.y < 0) return;
+
+    const clicked = new Point(coordinates.x, coordinates.y);
+    const firstClicked = shapes.find(shape => shape.type === 'ClickedPoint') as Point | undefined;
+
+    if (!firstClicked) {
+      const coloredAndClicked = clicked.setType('ClickedPoint').setColor('red');
+      setShapes([...shapes, coloredAndClicked]);
+      return;
     }
+
+    const shapesWithoutClicked = shapes.filter(shape => shape.type !== 'clickedPoint');
+    setShapes([
+      ...shapesWithoutClicked,
+      clicked.setColor('green'),
+      firstClicked.setType('Point').setColor('green'),
+      new Geodesic(firstClicked, clicked)
+    ]);
   }
 
-  return <canvas ref={canvasRef} onMouseMove={displayCursorCoordinates} onClick={addLineWithClick} />;
+  return <canvas ref={canvasRef} onMouseMove={displayCursorCoordinates} onClick={handleClick} />;
 }
 
-function getCoordFromMouse(canvas: HTMLCanvasElement, event: React.MouseEvent<HTMLCanvasElement>) {
-  return {
-    x: event.clientX - canvas.width / 2,
-    y: Math.floor(canvas.height * 0.95 - event.clientY),
-    canvasX: event.clientX,
-    canvasY: event.clientY,
-  }
-}
+// function getCoordFromMouse(canvas: HTMLCanvasElement, event: React.MouseEvent<HTMLCanvasElement>) {
+//   return {
+//     x: event.clientX - canvas.width / 2,
+//     y: Math.floor(canvas.height * 0.95 - event.clientY),
+//     canvasX: event.clientX,
+//     canvasY: event.clientY,
+//   }
+// }
 
-function getCoordFromMath(canvas: HTMLCanvasElement, x: number, y: number) {
-  return {
-    x,
-    y,
-    canvasX: x + canvas.width / 2,
-    canvasY: y === Infinity ? 0 : Math.floor(canvas.height * 0.95 - y),
-  }
-}
+// function getCoordFromMath(canvas: HTMLCanvasElement, x: number, y: number) {
+//   return {
+//     x,
+//     y,
+//     canvasX: x + canvas.width / 2,
+//     canvasY: y === Infinity ? 0 : Math.floor(canvas.height * 0.95 - y),
+//   }
+// }
 
-function getGeodesic(canvas: HTMLCanvasElement, start: { type: string; [key: string]: any }, end: { type: string; [key: string]: any }) {
-  const geodesicValues = computeGeodesicValues(start, end);
+// function getGeodesic(canvas: HTMLCanvasElement, start: { type: string; [key: string]: any }, end: { type: string; [key: string]: any }) {
+//   const geodesicValues = computeGeodesicValues(start, end);
 
-  if (geodesicValues.type === 'vertical') {
-    return {
-      type: 'geodesicVertical',
-      start: getCoordFromMath(canvas, start.x, 0),
-      end: getCoordFromMath(canvas, start.x, Infinity),
-    }
-  }
+//   if (geodesicValues.type === 'vertical') {
+//     return {
+//       type: 'geodesicVertical',
+//       start: getCoordFromMath(canvas, start.x, 0),
+//       end: getCoordFromMath(canvas, start.x, Infinity),
+//     }
+//   }
   
-  if (geodesicValues.type === 'circle') {
-    return {
-      type: 'geodesicCircle',
-      center: getCoordFromMath(canvas, geodesicValues.center, 0),
-      radius: geodesicValues.radius,
-    }
-  }
-}
+//   if (geodesicValues.type === 'circle') {
+//     return {
+//       type: 'geodesicCircle',
+//       center: getCoordFromMath(canvas, geodesicValues.center, 0),
+//       radius: geodesicValues.radius,
+//     }
+//   }
+// }
 
-function computeGeodesicValues(start: { type: string; [key: string]: any }, end: { type: string; [key: string]: any }) {
-  const EPSILON = 1e-6;
+// function drawAxis(context: CanvasRenderingContext2D, shape: { type: string; [key: string]: any }) {
+//   context.strokeStyle = 'black';
+//   context.beginPath();
+//   context.moveTo(shape.start.x, shape.start.y);
+//   context.lineTo(shape.end.x, shape.end.y);
+//   context.stroke();
+// }
 
-  if (Math.abs(start.x - end.x) < EPSILON) {
-    if (Math.abs(start.y - end.y) < EPSILON) {
-      throw new Error('The two points are the same');
-    }
-    return {
-      type: 'vertical',
-      center: Infinity,
-      radius: Infinity
-    };
-  }
+// function drawCursor(context: CanvasRenderingContext2D, shape: { type: string; [key: string]: any }) {
+//   context.fillStyle = 'black';
+//   context.font = '20px Arial';
+//   context.fillText(`(${shape.x}, ${shape.y})`, shape.canvasX + 10, shape.canvasY - 10);
+// }
 
-  const [midpointX, midpointY] = [(start.x + end.x) / 2, (start.y + end.y) / 2];
-  const slope = (end.y - start.y) / (end.x - start.x);
-  if (Math.abs(slope) < EPSILON) {
-    return {
-      type: 'circle',
-      center: midpointX,
-      radius: Math.sqrt((start.x - midpointX) ** 2 + start.y ** 2),
-    };
-  }
+// function drawPoint(context: CanvasRenderingContext2D, shape: { type: string; [key: string]: any }) {
+//   context.fillStyle = shape.type === 'point' ? 'black' : 'red';
+//   context.beginPath();
+//   context.arc(shape.canvasX, shape.canvasY, 5, 0, 2 * Math.PI);
+//   context.fill();
+// }
 
-  const perpendicularSlope = -1 / slope;
-  const perpYIntercept = midpointY - perpendicularSlope * midpointX;
-  const center = -perpYIntercept / perpendicularSlope;
-  return {
-    type: 'circle',
-    center: center,
-    radius: Math.sqrt((start.x - center) ** 2 + start.y ** 2),
-  };
-}
-
-function drawAxis(context: CanvasRenderingContext2D, shape: { type: string; [key: string]: any }) {
-  context.strokeStyle = 'black';
-  context.beginPath();
-  context.moveTo(shape.start.x, shape.start.y);
-  context.lineTo(shape.end.x, shape.end.y);
-  context.stroke();
-}
-
-function drawCursor(context: CanvasRenderingContext2D, shape: { type: string; [key: string]: any }) {
-  context.fillStyle = 'black';
-  context.font = '20px Arial';
-  context.fillText(`(${shape.x}, ${shape.y})`, shape.canvasX + 10, shape.canvasY - 10);
-}
-
-function drawPoint(context: CanvasRenderingContext2D, shape: { type: string; [key: string]: any }) {
-  context.fillStyle = shape.type === 'point' ? 'black' : 'red';
-  context.beginPath();
-  context.arc(shape.canvasX, shape.canvasY, 5, 0, 2 * Math.PI);
-  context.fill();
-}
-
-function drawGeodesic(context: CanvasRenderingContext2D, shape: { type: string; [key: string]: any }) {
-  context.strokeStyle = 'black';
-  context.beginPath();
-  if (shape.type === 'geodesicVertical') {
-    context.moveTo(shape.start.canvasX, shape.start.canvasY);
-    context.lineTo(shape.end.canvasX, shape.end.canvasY);
-  } else {
-    context.arc(shape.center.canvasX, shape.center.canvasY, shape.radius, 0, Math.PI, true);
-  }
-  context.stroke();
-}
+// function drawGeodesic(context: CanvasRenderingContext2D, shape: { type: string; [key: string]: any }) {
+//   context.strokeStyle = 'black';
+//   context.beginPath();
+//   if (shape.type === 'geodesicVertical') {
+//     context.moveTo(shape.start.canvasX, shape.start.canvasY);
+//     context.lineTo(shape.end.canvasX, shape.end.canvasY);
+//   } else {
+//     context.arc(shape.center.canvasX, shape.center.canvasY, shape.radius, 0, Math.PI, true);
+//   }
+//   context.stroke();
+// }
