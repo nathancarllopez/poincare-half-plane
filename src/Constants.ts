ce59016006e905031f4ffbb65a3931bfc260e469
Nathan Lopez <nathancarllopez@gmail.com>
2024-11-26 23:25:23 -0600

    First commit

---
+++ src/Constants.ts
@@ -0,0 +1,13 @@
+// For Shapes
+export const POINT_COLOR: string = "black";
+export const POINT_RADIUS: number = 5;
+export const LABEL_COLOR: string = "black";
+export const LABEL_FONT: string = "20px Arial";
+export const AXIS_COLOR: string = "black";
+export const GEODESIC_COLOR: string = "green";
+
+// For Canvas
+export const CANVAS_BACKGROUND_COLOR: string = "white";
+
+// For math
+export const EPSILON: number = 1e-6;
