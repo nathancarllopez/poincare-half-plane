ce9bd44ac49e28f0f7538fe0a7fac34d8410f670
Nathan Lopez <nathancarllopez@gmail.com>
2024-12-01 16:24:36 -0600

    Added components made from MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css';
 import { useState } from 'react';
+
+// Custom components
 import Canvas from './Canvas.tsx';
 import Toolbar from './Toolbar.tsx';
 import ToolbarConfig from './ToolbarConfig';
+import DrawerWithFab from './DrawerWithFab.tsx';
+
+// Material UI
+import InfoIcon from '@mui/icons-material/Info';
+import HandymanIcon from '@mui/icons-material/Handyman';
+
+interface DrawerState {
+  infoOpen: boolean;
+  toolbarOpen: boolean;
+}
 
 function App() {
+  const [drawerState, setDrawerState] = useState<DrawerState>({ infoOpen: false, toolbarOpen: false });
+
+  function toggleDrawer(drawer: keyof DrawerState) {
+    const currDrawerState = drawerState[drawer];
+    const newDrawerState = { ...drawerState, [drawer]: !currDrawerState };
+    setDrawerState(newDrawerState);
+  }
+
   const [toolbarConfig, setToolbarConfig] = useState<ToolbarConfig>(new ToolbarConfig());
 
   function handleToolbarChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -17,11 +37,27 @@ function App() {
 
   return (
     <div className="appContainer">
-      {/* Infobar component goes here */}
+      <DrawerWithFab
+        icon={<InfoIcon fontSize='large' />}
+        content={<div>Info</div>}
+        anchor='left'
+        fabPosition='topLeft'
+        isOpen={drawerState.infoOpen}
+        toggleDrawer={() => toggleDrawer('infoOpen')}
+      ></DrawerWithFab>
+
       <Canvas toolbarConfig={toolbarConfig} />
-      <Toolbar onToolbarChange={handleToolbarChange} />
+
+      <DrawerWithFab
+        icon={<HandymanIcon fontSize='large' />}
+        content={<Toolbar />}
+        anchor='right'
+        fabPosition='topRight'
+        isOpen={drawerState.toolbarOpen}
+        toggleDrawer={() => toggleDrawer('toolbarOpen')}
+      ></DrawerWithFab>
     </div>
-  )
+  );
 }
 
 export default App
